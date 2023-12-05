4d9fcb744239c69e4999e789090a3b62c092b5ec
Kevin Kibbz <kibebekevin@gmail.com>
2023-12-05 12:03:47 +0300

    updated

---
+++ app/Models/MpesaTransaction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MpesaTransaction extends Model
+{
+    protected $guarded = array();
+    public $timestamps = false;
+}
